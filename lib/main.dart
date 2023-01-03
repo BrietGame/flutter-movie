c965d123d94e2514a9e589cae52848b22fbbec0c
BrietGame <alexis.briet2003@gmail.com>
2023-01-03 15:03:50 +0100

    Add search

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:search_page/search_page.dart';
 import 'dart:math';
+
+class Movie implements Comparable<Movie> {
+  final String title;
+  final int note;
+  final Image image;
+
+  const Movie(this.title, this.note, this.image);
+
+  @override
+  int compareTo(Movie other) => title.compareTo(other.title);
+
+  String getTitle() => title;
+
+  int getNote() => note;
+
+  Image getImage() => image;
+}
 
 void main() {
   runApp(const MyApp());
@@ -9,101 +27,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Films',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const Movies(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          //children:
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -116,7 +47,7 @@ class Movies extends StatefulWidget {
 }
 
 class _MoviesState extends State<Movies> {
-  final _listMovies = <Widget>[];
+  final _listMovies = [];
 
   int randomNote() {
     var random = Random();
@@ -140,7 +71,7 @@ class _MoviesState extends State<Movies> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Liste films"),
+        title: Text("Liste des films"),
       ),
       body: GridView.builder(
           padding: const EdgeInsets.all(8),
@@ -149,50 +80,72 @@ class _MoviesState extends State<Movies> {
             crossAxisSpacing: 8,
           ),
           itemBuilder: (context, i) {
-            if (i >= _listMovies.length) {
-              _listMovies.addAll(Iterable<Widget>.generate(10, (i) {
-                String titleMovie = generateTitle();
-                int note = randomNote();
-                Image image = generateImage();
+            final movie = Movie(generateTitle(), randomNote(), generateImage());
+            _listMovies.add(movie);
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MovieDetail(titleMovie: titleMovie, note: note, image: image))
-                    );
-                  },
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MovieDetail(titleMovie: movie.title, note: movie.note, image: movie.image))
+                );
+              },
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
                         Container(
                           child: ClipRRect(
                             borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(15.0),
                               topRight: Radius.circular(15.0),
                             ),
-                            child: image,
+                            child: movie.image,
                           ),
                         ),
                         ListTile(
-                          title: Text(titleMovie),
+                          title: Text(movie.title),
                           trailing: CircleAvatar(
-                            backgroundColor: (note >= 5 && note <= 7) ? Colors.yellow : (note > 7) ? Colors.green : Colors.red,
-                            child: Text(note.toString()),
+                            backgroundColor: (movie.note >= 5 && movie.note <= 7) ? Colors.yellow : (movie.note > 7) ? Colors.green : Colors.red,
+                            child: Text(movie.note.toString()),
                           ),
                         ),
                       ]
-                    )
-                 ),
-                );
-              }));
-            }
-
-            return _listMovies[i];
+                  )
+              ),
+            );
           }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.search),
+        onPressed: () => showSearch(
+            context: context,
+            delegate: SearchPage(
+                builder: (movie) => ListTile(
+                  title: Text(movie.title),
+                  trailing: CircleAvatar(
+                    backgroundColor: (movie.note >= 5 && movie.note <= 7) ? Colors.yellow : (movie.note > 7) ? Colors.green : Colors.red,
+                    child: Text(movie.note.toString()),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MovieDetail(titleMovie: movie.title, note: movie.note, image: movie.image))
+                    );
+                  },
+                ),
+                filter: (movie) => [movie.title],
+                items: _listMovies.toList(),
+                searchLabel: 'Rechercher un film',
+                suggestion: const Center(
+                  child: Text('Rechercher un film'),
+                ),
+                failure: const Center(
+                  child: Text('Aucun film trouvé'),
+                ),
+            ),
+        ),
       ),
     );
   }
